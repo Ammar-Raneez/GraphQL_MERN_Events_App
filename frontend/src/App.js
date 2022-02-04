3d3f1951a235c6ad6019e58daf1194706b9de9ce
ammar <ammarraneez@gmail.com>
2022-02-04 22:26:37 +0530

    feat: add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
-import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
 import Auth from './pages/Auth';
 import Booking from './pages/Booking';
 import Event from './pages/Event';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Redirect from="/" to="/auth" exact />
-      <Route path="/auth" component={Auth} />
-      <Route path="/events" component={Event} />
-      <Route path="/bookings" component={Booking} />
+      <Navigation />
+      <main className="main-content">
+        <Switch>
+          <Redirect from="/" to="/auth" exact />
+          <Route path="/auth" component={Auth} />
+          <Route path="/events" component={Event} />
+          <Route path="/bookings" component={Booking} />
+        </Switch>
+      </main>
     </BrowserRouter>
   );
 }
