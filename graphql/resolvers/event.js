ff2ffa1c7a36649c206c234979c47824af9fdad9
ammar <ammarraneez@gmail.com>
2022-02-04 10:46:47 +0530

    feat: add in-memory mutation

--- graphql/resolvers/event.js
+++ graphql/resolvers/event.js
@@ -1,9 +1,19 @@
+const events = [];
+
 module.exports = {
   events: () => {
-    return ['Sailing', 'All-night coding']
+    return events;
   },
   createEvent: (args) => {
-    const eventName = args.name;
-    return eventName;
+    const event = {
+      _id: Math.random().toString(),
+      title: args.event.title,
+      description: args.event.description,
+      price: +args.event.price,
+      date: new Date().toISOString(),
+    }
+
+    events.push(event);
+    return event;
   }
 }
