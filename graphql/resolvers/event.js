7d4734d1e9a50ed3ccf0e254d6b5efc6764d3c92
ammar <ammarraneez@gmail.com>
2022-02-04 14:52:02 +0530

    feat: add relationship between user and events

--- graphql/resolvers/event.js
+++ graphql/resolvers/event.js
@@ -1,18 +1,26 @@
 const Event = require('../../models/event');
+const User = require('../../models/user');
 
 module.exports = {
   events: () => {
     return Event.find();
   },
-  createEvent: (args) => {
+  createEvent: async (args) => {
+    const creator = '61fceafbfa783b3601b6641b';
     const event = new Event({
       title: args.event.title,
       description: args.event.description,
       price: +args.event.price,
       date: new Date(args.event.date),
+
+      // mongoose automatically converts this to ObjectId
+      creator
     });
 
     try {
+      const user = await User.findById(creator);
+      user.createdEvents.push(event);
+      await user.save();
       return event.save();
     } catch (err) {
       console.log(err);
