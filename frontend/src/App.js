b7889cbf4a69e1f69d41a23decd889374969904b
ammar <ammarraneez@gmail.com>
2022-02-04 22:17:30 +0530

    feat: add boiler template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import Auth from './pages/Auth';
+import Booking from './pages/Booking';
+import Event from './pages/Event';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Redirect from="/" to="/auth" exact />
+      <Route path="/auth" component={Auth} />
+      <Route path="/events" component={Event} />
+      <Route path="/bookings" component={Booking} />
+    </BrowserRouter>
   );
 }
 
