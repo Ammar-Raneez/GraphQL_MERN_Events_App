9b3c03a8771a2f7f424ca407a4f0437670f52e83
ammar <ammarraneez@gmail.com>
2022-02-05 10:26:59 +0530

    feat: create boiler template in event page

--- frontend/src/pages/Event.js
+++ frontend/src/pages/Event.js
@@ -1,7 +1,116 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { useStateValue } from '../context/auth-context';
+import Backdrop from '../components/Backdrop/Backdrop';
+import EventList from '../components/Events/EventList/EventList';
+import Modal from '../components/Modal/Modal';
+import Spinner from '../components/Spinner/Spinner';
+import './Event.css';
 
 function Event() {
-  return <div></div>;
+  const [creating, setCreating] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [events, setEvents] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [stateValue, dispatch] = useStateValue();
+
+  const titleElRef = useRef();
+  const priceElRef = useRef();
+  const dateElRef = useRef();
+  const descriptionElRef = useRef();
+
+  const modalCancelHandler = () => {
+    setCreating(false);
+    setSelectedEvent(null);
+  }
+
+  const modalConfirmHandler = () => {
+    setCreating(false);
+  }
+
+  const bookEventHandler = () => {
+
+  }
+
+  const startCreateEventHandler = () => {
+    setCreating(true);
+  }
+
+  const showDetailHandler = () => {
+
+  }
+
+  return (
+    <React.Fragment>
+      {(creating || selectedEvent) && <Backdrop />}
+      {creating && (
+        <Modal
+          title="Add Event"
+          canCancel
+          canConfirm
+          onCancel={modalCancelHandler}
+          onConfirm={modalConfirmHandler}
+          confirmText="Confirm"
+        >
+          <form>
+            <div className="form-control">
+              <label htmlFor="title">Title</label>
+              <input type="text" id="title" ref={titleElRef} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="price">Price</label>
+              <input type="number" id="price" ref={priceElRef} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="date">Date</label>
+              <input type="datetime-local" id="date" ref={dateElRef} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="description">Description</label>
+              <textarea
+                id="description"
+                rows="4"
+                ref={descriptionElRef}
+              />
+            </div>
+          </form>
+        </Modal>
+      )}
+      {selectedEvent && (
+        <Modal
+          title={selectedEvent.title}
+          canCancel
+          canConfirm
+          onCancel={modalCancelHandler}
+          onConfirm={bookEventHandler}
+          confirmText={stateValue?.user.token ? 'Book' : 'Confirm'}
+        >
+          <h1>{selectedEvent.title}</h1>
+          <h2>
+            ${selectedEvent.price} -{' '}
+            {new Date(selectedEvent.date).toLocaleDateString()}
+          </h2>
+          <p>{selectedEvent.description}</p>
+        </Modal>
+      )}
+      {stateValue?.user.token && (
+        <div className="events-control">
+          <p>Share your own Events!</p>
+          <button className="btn" onClick={startCreateEventHandler}>
+            Create Event
+          </button>
+        </div>
+      )}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <EventList
+          events={events}
+          authUserId={stateValue?.user.userId}
+          onViewDetail={showDetailHandler}
+        />
+      )}
+    </React.Fragment>
+  );
 }
 
 export default Event;
