d0957c583def5d3aa206c8b34c459301ff4a786f
ammar <ammarraneez@gmail.com>
2022-02-05 12:22:51 +0530

    feat: get bookings

--- frontend/src/pages/Booking.js
+++ frontend/src/pages/Booking.js
@@ -1,7 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useStateValue } from '../context/auth-context';
+// import BookingsChart from '../components/Bookings/BookingChart/BookingChart';
+import BookingsControls from '../components/Bookings/BookingControls/BookingControls';
+import BookingList from '../components/Bookings/BookingList/BookingList';
+import Spinner from '../components/Spinner/Spinner';
+import { BASE_URL } from '../utils/util';
 
 function Booking() {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [outputType, setOutputType] = useState('list');
+  const [bookings, setBookings] = useState([]);
+  const [stateValue] = useStateValue();
+
+  useEffect(() => {
+    setIsLoading(true);
+    const getEvents = async () => {
+      const requestBody = {
+        query: `
+          query {
+            bookings {
+              _id
+              createdAt
+              event {
+                _id
+                title
+                date
+                price
+              }
+            }
+          }
+        `
+      };
+
+      try {
+        const response = await fetch(BASE_URL, {
+          method: 'POST',
+          body: JSON.stringify(requestBody),
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + stateValue?.user.token
+          }
+        });
+
+        const responseData = await response.json();
+        setBookings(responseData.data.bookings);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        throw new Error(err);
+      }
+    }
+
+    getEvents();
+  }, [stateValue?.user.token]);
+
+  const changeOutputTypeHandler = (outputType) => {
+    if (outputType === 'list') {
+      setOutputType('list');
+    } else {
+      setOutputType('chart');
+    }
+  };
+
+  const deleteBookingHandler = () => {
+
+  }
+
+  let content = <Spinner />;
+  if (!isLoading) {
+    content = (
+      <React.Fragment>
+        <BookingsControls
+          activeOutputType={outputType}
+          onChange={changeOutputTypeHandler}
+        />
+        <div>
+          {outputType === 'list' ? (
+            <BookingList
+              bookings={bookings}
+              onDelete={deleteBookingHandler}
+            />
+          ) : (
+            <></>
+            // <BookingsChart bookings={bookings} />
+          )}
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <React.Fragment>{content}</React.Fragment>
+  );
 }
 
 export default Booking;
